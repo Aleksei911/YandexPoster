2fc2d19632a29725eb4e18f4d556077fa118ff9a
Aleksei Kohnuk <kohnuk92@gmail.com>
2023-03-29 11:47:18 +0300

    Add model for images

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -6,3 +6,10 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='places/static/img/')
+
+    # def __str__(self):
+    #     return self.image
